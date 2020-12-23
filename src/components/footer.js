8e276fd208e00d7e9aef7204d1ce519d003b0152
Nam Nguyen <namnguyen.sci@gmail.com>
2020-12-23 17:14:15 +0700

    Fix className, htmlFor

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,9 +4,9 @@ import imgSnapSeed from '../assets/images/snapseed256.png';
 
 const Footer = () => {
   return (
-    <footer class="gradient-half-primary-body-v1 py-4">
-        <div class="container">
-            <div class="row justify-content-md-between font-size-1 py-3" 
+    <footer className="gradient-half-primary-body-v1 py-4">
+        <div className="container">
+            <div className="row justify-content-md-between font-size-1 py-3" 
                 style={{
                     // backgroundImage: "url(" + imgWorldMap + ")",
                     backgroundImage: `url(${imgWorldMap})`,
@@ -15,20 +15,20 @@ const Footer = () => {
                     backgroundPositionX: '0px',
                     backgroundPositionY: '30%'
                 }}>
-                <div class="col-lg-3 mb-4 mb-lg-0">
-                    <div class="d-flex h-100 flex-column">
-                        <div class="d-flex align-items-center mb-4">
+                <div className="col-lg-3 mb-4 mb-lg-0">
+                    <div className="d-flex h-100 flex-column">
+                        <div className="d-flex align-items-center mb-4">
                             <img src={imgSnapSeed} width="32" alt="Landing Page Logo"/>
-                            <span class="h4 text-white mb-0 ml-3">Powered by Nambk</span>
+                            <span className="h4 text-white mb-0 ml-3">Powered by Nambk</span>
                         </div>
-                        <p class="text-white">A brand new journey to the beautiful angles of this world. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+                        <p className="text-white">A brand new journey to the beautiful angles of this world. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
 
-                        <div class="d-flex align-items-center">
-                            <a class="btn btn-xss btn-soft-light mr-2" href="/settings">
-                                <i class="fa fa-cogs mr-1"></i>Preferences
+                        <div className="d-flex align-items-center">
+                            <a className="btn btn-xss btn-soft-light mr-2" href="/settings">
+                                <i className="fa fa-cogs mr-1"></i>Preferences
                             </a>
-                            <button id="darkModaBtn" type="button" data-toggle="tooltip" data-title="Day/Night Mode" class="btn btn-sm btn-icon btn-soft-light" data-original-title="" title="">
-                                <i id="darkModaBtnIcon" class="fa fa-moon"></i></button>
+                            <button id="darkModaBtn" type="button" data-toggle="tooltip" data-title="Day/Night Mode" className="btn btn-sm btn-icon btn-soft-light" data-original-title="" title="">
+                                <i id="darkModaBtnIcon" className="fa fa-moon"></i></button>
                         </div>
 
                     </div>
--- src/components/header.js
+++ src/components/header.js
@@ -10,43 +10,44 @@ const Header = () => {
   return (
   <ThemeProvider theme={theme === 'light' ? themes.lightTheme : themes.darkTheme}>
     <GlobalStyles />
-    <nav id="ac-localnav" class="js touch css-sticky ac-ln-sticking" lang="en-US" dir="ltr" role="navigation" aria-label="Local">
-        <div class="ac-ln-wrapper">
-            <div class="ac-ln-background"></div>
-            <div class="ac-ln-content">
-                <div class="ac-ln-title">
-                    <a class="border-radius-50" href="/nambk"><img src={imgLogo} alt="logo" /></a>
+    <nav id="ac-localnav" className="js touch css-sticky ac-ln-sticking" lang="en-US" dir="ltr" role="navigation" aria-label="Local">
+        <div className="ac-ln-wrapper">
+            <div className="ac-ln-background"></div>
+            <div className="ac-ln-content">
+                <div className="ac-ln-title">
+                    <a className="border-radius-50" href="/nambk"><img src={imgLogo} alt="logo" /></a>
                 </div>
-                <div class="ac-ln-menu">
-                    <a href="/nambk" class="ac-ln-menucta-anchor ac-ln-menucta-anchor-open" id="ac-ln-menustate-open">
-                        <span class="ac-ln-menucta-anchor-label">Local Nav Open Menu</span>
+                <div className="ac-ln-menu">
+                    <a href="/nambk" className="ac-ln-menucta-anchor ac-ln-menucta-anchor-open" id="ac-ln-menustate-open">
+                        <span className="ac-ln-menucta-anchor-label">Local Nav Open Menu</span>
                     </a>
-                    <a href="/nambk" class="ac-ln-menucta-anchor ac-ln-menucta-anchor-close" id="ac-ln-menustate-close">
-                        <span class="ac-ln-menucta-anchor-label">Local Nav Close Menu</span>
+                    <a href="/nambk" className="ac-ln-menucta-anchor ac-ln-menucta-anchor-close" id="ac-ln-menustate-close">
+                        <span className="ac-ln-menucta-anchor-label">Local Nav Close Menu</span>
                     </a>
-                    <div class="ac-ln-menu-tray">
-                        <ul class="ac-ln-menu-items">
-                            <li class="ac-ln-menu-item">
-                                <span class="ac-ln-menu-link current" role="link" aria-disabled="true" aria-current="page">Home</span>
+                    <div className="ac-ln-menu-tray">
+                        <ul className="ac-ln-menu-items">
+                            <li className="ac-ln-menu-item">
+                                <span className="ac-ln-menu-link current" role="link" aria-disabled="true" aria-current="page">Home</span>
                             </li>
-                            <li class="ac-ln-menu-item">
-                                <a href="/nambk" class="ac-ln-menu-link">Design</a>
+                            <li className="ac-ln-menu-item">
+                                <a href="/nambk" className="ac-ln-menu-link">Design</a>
                             </li>
-                            <li class="ac-ln-menu-item">
-                                <a href="/nambk" class="ac-ln-menu-link">Products</a>
+                            <li className="ac-ln-menu-item">
+                                <a href="/nambk" className="ac-ln-menu-link">Products</a>
                             </li>
                         </ul>
                     </div>
-                    <div class="ac-ln-actions">
-                        <div class="ac-ln-action ac-ln-action-menucta" aria-hidden="true">
-                            <label for="ac-ln-menustate" class="ac-ln-menucta">
-                                <span class="ac-ln-menucta-chevron"></span>
+                    <div className="ac-ln-actions">
+                        <div className="ac-ln-action ac-ln-action-menucta" aria-hidden="true">
+                            <label htmlFor="ac-ln-menustate" className="ac-ln-menucta">
+                                <span className="ac-ln-menucta-chevron"></span>
                             </label>
                         </div>
-                        <div class="ac-ln-action ac-ln-action-button">
-                            <a class="ac-ln-button" href="/nambk">Login <span class="ac-ln-action-product"></span></a>
+                        <div className="ac-ln-action ac-ln-action-button">
+                            <a className="ac-ln-button" href="/nambk">Login </a>
+                            <a className="ac-ln-button" onClick={toggleTheme}><i className="fa fa-moon"></i></a>
+                            <a className="btn btn-sm btn-icon btn-soft-light" onClick={toggleTheme}><i className="fa fa-sun-o"></i></a>
                         </div>
-                        <button onClick={toggleTheme}>Toggle Theme</button>
                     </div>
                 </div>
             </div>
