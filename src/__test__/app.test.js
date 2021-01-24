06e55cebef16751993d57af963a62758108aee75
Nam Nguyen <namnguyen.sci@gmail.com>
2021-01-25 02:31:18 +0700

    Add test cases for props change (3 cases)

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -14,11 +14,40 @@ describe('App:', () => {
     expect(wrapper.find('.container-fluid')).toHaveLength(1);
     expect(wrapper.find(PrimeForm)).toHaveLength(1);
     expect(wrapper.find(PrimesForm)).toHaveLength(1);
-    expect(wrapper.find(Footer)).toHaveLength(1); // for Jest
-    // expect(wrapper.find(Foo)).to.have.length(3); // for Chai Assertion Library
-    // expect(wrapper.find(Foo).length).toBe(1); // for Enzyme
+    expect(wrapper.find(Footer)).toHaveLength(1); 
     expect(wrapper).toMatchSnapshot();
     // console.log(wrapper.debug());
+  });
+
+  // https://medium.com/@acesmndr/testing-react-functional-components-with-hooks-using-enzyme-f732124d320a
+  it('changes props when executing by calling them as functions with passed param 1', () => {
+    const wrapper = shallow(<App />); 
+    wrapper.find('Footer').prop('toggleTheme')({
+      target: {
+        theme: 'light'
+      }
+    });
+    expect(wrapper.find('Footer').prop('theme')).toEqual('dark');
+  });
+
+  it('changes props when executing by calling them as functions with passed param 2', () => {
+    const wrapper = shallow(<App />); 
+    wrapper.find('Footer').prop('toggleTheme')({
+      target: {
+        theme: 'dark'
+      }
+    });
+    expect(wrapper.find('Footer').prop('theme')).toEqual('moon');
+  });
+
+  it('changes props when executing by calling them as functions with passed param 3', () => {
+    const wrapper = shallow(<App />); 
+    wrapper.find('Footer').prop('toggleTheme')({
+      target: {
+        theme: 'moon'
+      }
+    });
+    expect(wrapper.find('Footer').prop('theme')).toEqual('light');
   });
 
 });
