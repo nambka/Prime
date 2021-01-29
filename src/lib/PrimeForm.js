412b9b80fa6c189ebeb1b78f8a6247d562f8b397
Nam Nguyen <namnguyen.sci@gmail.com>
2021-01-29 23:59:40 +0700

    Convert to arrow function component

--- src/lib/PrimeForm.js
+++ src/lib/PrimeForm.js
@@ -1,49 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { calcPrime } from './Algo'
 
 // Render the input form to enter a number
-// https://reactjs.org/docs/forms.html#controlled-components
-class PrimeForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputNum: '',
-      answer: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+const PrimeForm = () => {
+  const [inputNum, setInputNum] = useState('');
+  const [answer, setAnswer] = useState('');
+
+  const handleChange = (e) => {
+    setInputNum(e.target.value);
   }
 
-  handleChange(e) {
-    this.setState({
-      inputNum: e.target.value
-    });
-  }
-
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.setState({
-      answer: calcPrime(this.state.inputNum),
-    });  
+    setAnswer(calcPrime(inputNum));  
   }
 
-  render() {
-    return (
-      <form id="frmFindHighestPrime" onSubmit={this.handleSubmit} className="container-fluid nambk-container-input text-center">
-        <h2 className="nambk-header">Highest Prime</h2>
-        <div className="nambk-header-desc">Lower than Input Number</div>
-        <div>
-          <input id="inputNumber" name="inputNumber" className="nambk-input" style={{width: '50%'}} 
-                 type="number" value={this.state.inputNum} aria-label="Input Number" autoFocus
-                 onChange={this.handleChange} placeholder="Enter a number"/>
-          <input id="btnFindHighestPrime" className="nambk-btn nambk-btn-primary" 
-                 type="submit" value="Go"/>
-        </div>
-        <div id="result" className="mt-3" readOnly style={{minHeight: '2rem',}}>
-          {this.state.answer}  
-        </div>   
-      </form>
-    );
-  }
+  return (
+    <form id="frmFindHighestPrime" onSubmit={handleSubmit} className="container-fluid nambk-container-input text-center">
+      <h2 className="nambk-header">Highest Prime</h2>
+      <div className="nambk-header-desc">Lower than Input Number</div>
+      <div>
+        <input id="inputNumber" name="inputNumber" className="nambk-input" style={{width: '50%'}} 
+                type="number" value={inputNum} aria-label="Input Number" autoFocus
+                onChange={handleChange} placeholder="Enter a number"/>
+        <input id="btnFindHighestPrime" className="nambk-btn nambk-btn-primary" 
+                type="submit" value="Go"/>
+      </div>
+      <div id="result" className="mt-3" readOnly style={{minHeight: '2rem',}}>
+        {answer}  
+      </div>   
+    </form>
+  );
 }
 export default PrimeForm;
--- src/lib/PrimesForm.js
+++ src/lib/PrimesForm.js
@@ -1,73 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { sieve } from './Algo'
 
 // Render the input form to enter two numbers
-class PrimesForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputFirstNum: 1,
-      inputLastNum: 1000,
-      answer: ['Primes']
-    };
-    // this.onFirstNumChange = this.onFirstNumChange.bind(this);
-    // this.onLastNumChange = this.onLastNumChange.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+// Multiple inputs: https://www.pluralsight.com/guides/handling-multiple-inputs-with-single-onchange-handler-react
+const PrimesForm = () => {
+  const [states, setState] = useState({
+    inputFirstNum: 1,
+    inputLastNum: 1000
+  });
+  const [answer, setAnswer] = useState(['Primes']);
 
-  // onFirstNumChange(e) {
-  //   this.setState({
-  //     inputFirstNum: e.target.value
-  //   })
-  // }
-
-  // onLastNumChange(e) {
-  //   this.setState({
-  //     inputLastNum: e.target.value
-  //   })
-  // }
-
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
+  const handleChange = (e) => {
+    const val = e.target.value;
+    setState({
+      ...states,
+      [e.target.name]: val
     });
   }
 
-  handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const num1 = Number(this.state.inputFirstNum)
-    const num2 = Number(this.state.inputLastNum)
+    const num1 = Number(states.inputFirstNum)
+    const num2 = Number(states.inputLastNum)
     const max = num1 > num2 ? num1 : num2
     const min = max === num1 ? num2 : num1
-
-    this.setState({
-      answer: sieve(max, min),
-    });   
+    setAnswer(sieve(max, min));   
   }
 
-  render() {
-    return (
-      <form id="frmFindPrimes" onSubmit={this.handleSubmit} className="container-fluid nambk-container-input">
-        <h2 className="nambk-header">Prime Numbers</h2>
-        <div className="nambk-header-desc">Between Two Numbers</div>
-        <div>
-          <input className="nambk-input" style={{width: '33%'}} 
-                 type="number" min="1" max="9999999" aria-label="Input Number"
-                 value={this.state.inputFirstNum} onChange={this.handleChange} 
-                 id="inputFirstNum" name="inputFirstNum" placeholder="Enter a number"/>
-          <input className="nambk-input" style={{width: '33%'}} 
-                 type="number" min="1" max="9999999" aria-label="Input Number"
-                 value={this.state.inputLastNum} onChange={this.handleChange} 
-                 id="inputLastNum" name="inputLastNum" placeholder="Enter a number"/>
-          <input id="btnFindPrimes" className="nambk-btn nambk-btn-primary" 
-                 type="submit" value="Go"/>
-        </div>
-        <div id="result" className="mt-3" style={{minHeight: '4rem',}}>
-          {this.state.answer.join(', ')}
-        </div>   
-      </form>
-    );
-  }
+  return (
+    <form id="frmFindPrimes" onSubmit={handleSubmit} className="container-fluid nambk-container-input">
+      <h2 className="nambk-header">Prime Numbers</h2>
+      <div className="nambk-header-desc">Between Two Numbers</div>
+      <div>
+        <input className="nambk-input" style={{width: '33%'}} 
+                type="number" min="1" max="9999999" aria-label="Input Number"
+                value={states.inputFirstNum} onChange={handleChange} 
+                id="inputFirstNum" name="inputFirstNum" placeholder="Enter a number"/>
+        <input className="nambk-input" style={{width: '33%'}} 
+                type="number" min="1" max="9999999" aria-label="Input Number"
+                value={states.inputLastNum} onChange={handleChange} 
+                id="inputLastNum" name="inputLastNum" placeholder="Enter a number"/>
+        <input id="btnFindPrimes" className="nambk-btn nambk-btn-primary" 
+                type="submit" value="Go"/>
+      </div>
+      <div id="result" className="mt-3" style={{minHeight: '4rem',}}>
+        {answer.join(', ')}
+      </div>   
+    </form>
+  );
 }
 export default PrimesForm;
