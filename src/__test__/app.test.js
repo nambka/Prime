9721df51ffb0feecc41ed28fdad53abe92281cc6
Nam Nguyen <namnguyen.sci@gmail.com>
2021-01-29 21:26:32 +0700

    Correct imports

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import App from '../components/app';
-import Header from '../components/header';
-import Footer from '../components/header';
+import App from '../components/App';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import PrimeForm from '../lib/PrimeForm';
 import PrimesForm from '../lib/PrimesForm';
 import { shallow, mount } from 'enzyme';
