ca0870a736fe8c970564bf0442d786ca7b6f7e05
Nam Nguyen <namnguyen.sci@gmail.com>
2021-01-30 02:20:13 +0700

    Revise and rename import from footer to Footer

--- src/__test__/Footer.test.js
+++ src/__test__/Footer.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Footer from '../components/footer';
+import Footer from '../components/Footer';
 import { shallow, mount } from 'enzyme';
 
 describe('Footer', () => {
